9a37650e94999032e06dedc4820eebcff0f95bcd
Leeford Nduru <leemnduru@gmail.com>
2020-03-17 20:47:36 -0400

    Add files via upload

---
+++ src/pcb.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+
+public class PCB
+{
+
+	double cpu_id,prog_num,prog_count,code_size;
+	char[] state={"ready","running","waiting","halted"};
+	double[] registers;
+	double memory_limit;
+	double [] resources;
+
+	public PCB(double a,char b,double[]c, double d, double[]e)
+	{
+		prog_num=a;
+		state=b;
+		registers=c;
+		memory_limit=d;
+		resources=e;
+	}
+
+
+}
